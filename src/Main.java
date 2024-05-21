73985a70b8e15da35700810ac7d549f7f1cbedd4
Dmitry <dkonchik@gmail.com>
2024-05-21 22:52:05 +0300

    Fourth task

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.time.Instant;
 import java.time.LocalDate;
 import java.util.Scanner;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.regex.Matcher;
@@ -17,7 +18,8 @@ public class Main {
 //        positiveNumbers(num);
         //Third task
         System.out.println(convertMoney("dsg100 BYN123", 3.2));
-
+        //Fourth task
+        convertMoneyAndPrint("dsg100 BYN123", 3.2);
     }
 
     static void whenYouHas100Birthday() {
@@ -59,17 +61,23 @@ public class Main {
 
     static double convertMoney(String money, double exchangeRate) {
         //Third task
-//        String str = money.replaceAll("\\s+\\w+", "");
-        String str = trimStr(money);
-        Function<String, Double> convert = Double::parseDouble;
-        return convert.apply(str) * exchangeRate;
+        Function<String, Double> convert = m -> Double.parseDouble(trimStr(m)) * exchangeRate;
+        return convert.apply(money);
     }
 
     static String trimStr(String money) {
         //Third task
+        //Fourth task
         Pattern pattern = Pattern.compile("[0-9]+");
         Matcher matcher = pattern.matcher(money);
         matcher.find();
         return matcher.group();
+    }
+
+    static void convertMoneyAndPrint(String money, double exchangeRate) {
+        //Fourth task
+//        Consumer<String> convert = m -> System.out.println(Double.parseDouble(trimStr(m)) * exchangeRate + " USD");
+        Consumer<String> convert = m -> System.out.println(convertMoney(money, exchangeRate) + " USD");
+        convert.accept(money);
     }
 }
